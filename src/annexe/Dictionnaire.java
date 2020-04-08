ae2d7d2393c23f04726c9846dbea77761d379226
Tristan <tristan.pin81@gmail.com>
2020-04-08 13:07:07 +0200

    v0.0.8

---
+++ src/annexe/Dictionnaire.java
@@ -0,0 +1,46 @@
+package annexe;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Hashtable;
+
+public class Dictionnaire extends Hashtable<Integer,String> {
+
+    // VARIABLES
+    // vide
+
+    // CONSTANTES
+    // vide
+
+    // CONSTRUCTEUR
+    public Dictionnaire() throws IOException {
+
+        this.initialisation();
+    }
+
+    // METHODES
+    public void initialisation() throws IOException{
+
+        // OUVERTURE DU DICTIONNAIRE
+        BufferedReader in = new BufferedReader(new FileReader("dictionnaire.txt"));
+        String line;
+
+        // AJOUT DE CHAQUE MOT DANS LA HASTABLE
+        while ((line = in.readLine()) != null) {
+
+   			this.put(line.hashCode(),line);
+            }
+            
+        // FERMETURE DU FICHIER
+		in.close();
+    }
+
+    // TEST
+    public static void main(String[] args) throws IOException{
+
+        Dictionnaire test = new Dictionnaire();
+        System.out.println(test.containsValue("yoloaque"));
+    }
+}
--- src/annexe/Pioche.java
+++ src/annexe/Pioche.java
@@ -3,14 +3,13 @@ package annexe;
 import java.util.Hashtable;
 import java.util.Random;
 
-public class Pioche {
+public class Pioche extends Hashtable<Character,int[]> {
 
     // VARIABLES
-    private Hashtable<Character,int[]> sac= new Hashtable<Character,int[]>();
     private int n_pieces = 0;
 
     // CONSTANTES
-
+    // vide
 
     // CONSTRUCTEUR
     public Pioche(){
@@ -21,33 +20,33 @@ public class Pioche {
     // METHODES
     public void initialisation(){
         
-        this.sac.put('A', new int[] {1, 9});
-        this.sac.put('B', new int[] {3, 2});
-        this.sac.put('C', new int[] {3, 2});
-        this.sac.put('D', new int[] {2, 3});
-        this.sac.put('E', new int[] {1, 15});
-        this.sac.put('F', new int[] {4, 2});
-        this.sac.put('G', new int[] {2, 2});
-        this.sac.put('H', new int[] {4, 2});
-        this.sac.put('I', new int[] {1, 8});
-        this.sac.put('J', new int[] {8, 1});
-        this.sac.put('K', new int[] {10, 1});
-        this.sac.put('L', new int[] {1, 5});
-        this.sac.put('M', new int[] {2, 3});
-        this.sac.put('N', new int[] {1, 6});
-        this.sac.put('O', new int[] {1, 6});
-        this.sac.put('P', new int[] {3, 2});
-        this.sac.put('Q', new int[] {8, 1});
-        this.sac.put('R', new int[] {1, 6});
-        this.sac.put('S', new int[] {1, 6});
-        this.sac.put('T', new int[] {1, 6});
-        this.sac.put('U', new int[] {1, 6});
-        this.sac.put('V', new int[] {4, 2});
-        this.sac.put('W', new int[] {10, 1});
-        this.sac.put('X', new int[] {10, 1});
-        this.sac.put('Y', new int[] {10, 1});
-        this.sac.put('Z', new int[] {10, 1});
-        this.sac.put('[', new int[] {0, 2});
+        this.put('A', new int[] {1, 9});
+        this.put('B', new int[] {3, 2});
+        this.put('C', new int[] {3, 2});
+        this.put('D', new int[] {2, 3});
+        this.put('E', new int[] {1, 15});
+        this.put('F', new int[] {4, 2});
+        this.put('G', new int[] {2, 2});
+        this.put('H', new int[] {4, 2});
+        this.put('I', new int[] {1, 8});
+        this.put('J', new int[] {8, 1});
+        this.put('K', new int[] {10, 1});
+        this.put('L', new int[] {1, 5});
+        this.put('M', new int[] {2, 3});
+        this.put('N', new int[] {1, 6});
+        this.put('O', new int[] {1, 6});
+        this.put('P', new int[] {3, 2});
+        this.put('Q', new int[] {8, 1});
+        this.put('R', new int[] {1, 6});
+        this.put('S', new int[] {1, 6});
+        this.put('T', new int[] {1, 6});
+        this.put('U', new int[] {1, 6});
+        this.put('V', new int[] {4, 2});
+        this.put('W', new int[] {10, 1});
+        this.put('X', new int[] {10, 1});
+        this.put('Y', new int[] {10, 1});
+        this.put('Z', new int[] {10, 1});
+        this.put('[', new int[] {0, 2});
 
         this.n_pieces = 102;
     }
@@ -62,18 +61,18 @@ public class Pioche {
             int num_lettre = 1 + r.nextInt(this.n_pieces); 
 
             // INITIALISATION DES VARIABLES
-            int indice = 65; char lettre = (char) indice; int somme = sac.get(lettre)[1];
+            int indice = 65; char lettre = (char) indice; int somme = this.get(lettre)[1];
 
             // RECHERCHE DE LA LETTRE
             while (somme < num_lettre) {
 
                 indice ++; lettre = (char) indice;
-                somme += sac.get(lettre)[1];
+                somme += this.get(lettre)[1];
             }
 
             // MISE A JOUR DES VARIABLES DE CLASSE
-            int[] auxiliaire = sac.get(lettre);
-            sac.put(lettre, new int[] { auxiliaire [0], auxiliaire [1] - 1});
+            int[] auxiliaire = this.get(lettre);
+            this.put(lettre, new int[] { auxiliaire [0], auxiliaire [1] - 1});
             this.n_pieces --;
 
             // REMISE DE LA LETTRE
@@ -90,14 +89,14 @@ public class Pioche {
     @Override
     public String toString(){
 
-        String inventaire = "\n";
+        String txt = "\n";
 
         for (int i = 65; i < 65 + 27; i++){
 
-            inventaire += "Lettre : " + (char) i + " infos : " + sac.get((char) i)[0] + ", " + sac.get((char) i)[1] + "\n";
+            txt += "Lettre : " + (char) i + " infos : " + this.get((char) i)[0] + ", " + this.get((char) i)[1] + "\n";
         }
 
-        return inventaire;
+        return txt;
     }
 
     // TEST
